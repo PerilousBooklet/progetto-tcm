281d02eef7b5936c1d0127d6d517727dc4d00f06
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-08-21 12:44:25 +0200

    Added setup script for linux.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# https://blogs.igalia.com/jaragunde/2023/12/setting-up-a-minimal-command-line-android-emulator-on-linux/
+# 
+
+# SDK Install
+# paru -S android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-platform android-tools
+
+# Android Emulator Install
+# paru -S android-emulator
+
+# Android OS Images Install
+# https://aur.archlinux.org/packages?K=android-+system+image
+# paru -S android-x86-64-system-image
+
+# Create new project
+flutter create mobile
