5822787b1df28749d9f7099c597a2c4cee819270
socket772 <socket772@protonmail.com>
2024-09-04 20:11:20 +0200

    Rifatta pagina di login

---
+++ unibg_mobile_and_cloud_2024-main/mytedx/lib/pages/profile_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProfilePage();
+  }
+}
+
+class _ProfilePage extends State<ProfilePage> {
+  bool _isTextFieldEnabled = true;
+  final _controller = TextEditingController();
+
+  loginPress() {
+    setState(() {
+      _isTextFieldEnabled = false;
+    });
+  }
+
+  logoutPress() {
+    setState(() {
+      _controller.text = "";
+      _isTextFieldEnabled = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            height: 150.0,
+            width: MediaQuery.of(context).size.width - 20,
+            padding: const EdgeInsets.only(top: 0),
+            child: Column(
+              children: [
+                // Username Field
+                Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      enabled: _isTextFieldEnabled,
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'User Name',
+                          hintText: 'Enter name'),
+                    )),
+                Row(
+                  children: [
+                    OutlinedButton(
+                        onPressed: loginPress, child: const Text("Login")),
+                    const Padding(padding: EdgeInsets.all(10)),
+                    OutlinedButton(
+                        onPressed: logoutPress, child: const Text("Logout"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
--- unibg_mobile_and_cloud_2024-main/mytedx/lib/ui/talks_list.dart
+++ unibg_mobile_and_cloud_2024-main/mytedx/lib/ui/talks_list.dart
@@ -9,55 +9,15 @@ import 'package:mytedx/ted_repository.dart';
 // }
 
 class TalksList extends StatelessWidget {
-  final _scrollController = ScrollController();
-
   TalksList({super.key});
 
   @override
   Widget build(BuildContext context) {
     // ?
-    TedRepository ted_repo = TedRepository();
-    // Future<List<TalkMainPage>>talks_list_future = ted_repo.getTalkList();
-    // List<TalkMainPage> talks_list = List.empty();
-    // talks_list_future.then((result){
-    //   setState(() {
-    //     talks_list = result;
-    //     print(result);
-    //   });
-    // });
+    TedRepository tedRepo = TedRepository();
 
-    // ?
-    // return ListView.builder(
-    //   padding: const EdgeInsets.all(8),
-    //   itemCount: talks_list.length,
-    //   scrollDirection: Axis.vertical,
-    //   shrinkWrap: true,
-    //   itemBuilder: (BuildContext context, int index) {
-    //     return Container(
-    //       height: 50,
-    //       // Talk entry
-    //       child: Center(
-    //         child: Padding(
-    //           padding: EdgeInsets.all(10),
-    //           child: Row(
-    //             children: [
-    //               Image.network(talks_list[index].img_url),
-    //               Column(
-    //                 children: [
-    //                   Text(talks_list[index].title, style: TextStyle(fontSize: 23),),
-    //                   Text(talks_list[index].description, style: TextStyle(fontSize: 20),),
-    //                 ],
-    //               )
-    //             ],
-    //           ),
-    //         ),
-    //       ),
-    //     );
-    //   }
-    // );
-    // ?
     return FutureBuilder<List<TalkMainPage>>(
-      future: ted_repo.getTalkList(),
+      future: tedRepo.getTalkList(),
       builder: (_, AsyncSnapshot<List<TalkMainPage>> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           if (snapshot.hasData) {
