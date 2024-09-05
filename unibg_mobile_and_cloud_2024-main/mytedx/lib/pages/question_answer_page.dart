022cccee63b5a0f0fbd8541e2f324994d54261d6
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-09-05 23:47:16 +0200

    Aggiunto scheletro della pagina risposta.

--- unibg_mobile_and_cloud_2024-main/mytedx/lib/pages/question_answer_page.dart
+++ unibg_mobile_and_cloud_2024-main/mytedx/lib/pages/question_answer_page.dart
@@ -4,13 +4,58 @@ class QuestionAnswerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('TedQuiz'),
-        ),
-        body: Expanded(
-          child: ListView(
-            children: [],
+    appBar: AppBar(
+      //title: const Text('TedQuiz'),
+    ),
+    body: Expanded(
+      child: ListView(
+        children: [
+          // Question Text
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: TextField(
+              maxLines: 10,
+              enabled: false,
+              decoration: const InputDecoration(border: OutlineInputBorder()),
+              style: const TextStyle(color: Colors.black),
+            ),
           ),
-        ));
+          // Answer Buttons
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                // True
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.lightGreen),
+                    onPressed: () => {},
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      enabled: false,
+                      style: const TextStyle(color: Colors.black),
+                    )
+                  ),
+                ),
+                // False
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () => {},
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      enabled: false,
+                      style: const TextStyle(color: Colors.black),
+                    )
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
